42f43cc310cc4407c9a90011ad71187f404bf3c0
Emmanuel Apau <kave@users.noreply.github.com>
2019-04-24 16:30:02 -0400

    LS command for querying secrets in parameter store - ignore docutils dependency on boto3 for license check

--- upside/enforcer/app.py
+++ upside/enforcer/app.py
@@ -1,9 +1,11 @@
 import click
 
 from upside.enforcer.upload.cli import upload
+from upside.enforcer.ls.cli import list_secrets
 
 
-@click.group(commands={'upload': upload}, help='UPLOAD - uploads secrets to AWS parameter store.')
+@click.group(commands={'upload': upload, 'ls': list_secrets}, help='UPLOAD - uploads secrets to AWS parameter store.\n\n' +
+                                                                   'LS - list secrets from AWS parameter store.\n')
 def enforcer():
     pass
 
--- upside/enforcer/util/secret.py
+++ upside/enforcer/util/secret.py
@@ -1,14 +1,23 @@
+from datetime import datetime
+
+
 class Secret:
     key: str
     value: str
     tags: list
     parent_directory: str
     name: str
+    version: str
+    last_modified: datetime
+    last_modified_user: str
 
-    def __init__(self, key: str, value: str, tags: list = list):
+    def __init__(self, key: str, value: str = None, tags: list = list, version: str = None, last_modified: datetime = None, last_modified_user: str = None):
         self.key = key
         self.value = value
         self.tags = tags
+        self.version = version
+        self.last_modified = last_modified
+        self.last_modified_user = last_modified_user
 
         result = key.split('/')
         self.parent_directory = result[1]
