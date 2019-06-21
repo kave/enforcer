d4a4040d39fbf52ff7d1dd2f55b3a81a8f8806b9
Emmanuel <kave@users.noreply.github.com>
2019-06-21 13:54:47 -0400

    Show command for displaying secrets (#17)
    * allow users to pass in a description, since parent directory is added to the tags

--- upside/enforcer/app.py
+++ upside/enforcer/app.py
@@ -2,10 +2,14 @@ import click
 
 from upside.enforcer.upload.cli import upload
 from upside.enforcer.ls.cli import list_secrets
+from upside.enforcer.show.cli import show_secret
 
 
-@click.group(commands={'upload': upload, 'ls': list_secrets}, help='UPLOAD - uploads secrets to AWS parameter store.\n\n' +
-                                                                   'LS - list secrets from AWS parameter store.\n')
+@click.group(commands={'upload': upload,
+                       'ls': list_secrets,
+                       'show': show_secret}, help='UPLOAD - uploads secrets to AWS parameter store.\n\n' +
+                                                  'LS - list secrets from AWS parameter store.\n' +
+                                                  'SHOW - Show secret details from AWS parameter store.\n')
 def enforcer():
     pass
 
